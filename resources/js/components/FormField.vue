f762ae71828d803fd4fba8af536ecb4673434f64
Dieter Stinglhamber <d.stinglhamber@altavia-act.com>
2018-11-26 10:27:33 +0100

    use span instead of input

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -1,15 +1,7 @@
 <template>
     <default-field :field="field">
         <template slot="field">
-             <input
-                :id="field.attribute"
-                :dusk="field.attribute"
-                :type="text"
-                v-model="value"
-                class="w-full form-control form-plain-text text-80 h-9 pt-2 ml-3"
-                readonly
-                disabled
-            />
+            <span class="inline-block pt-2 leading-tight">{{ field.value }}</span>
         </template>
     </default-field>
 </template>
@@ -18,6 +10,8 @@
 import { FormField } from 'laravel-nova';
 
 export default {
-  mixins: [FormField]
+    mixins: [FormField],
+
+    props: ['field'],
 };
 </script>
